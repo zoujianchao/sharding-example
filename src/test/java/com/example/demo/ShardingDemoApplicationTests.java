39ac1a8e3098a53ed5b8076aa3849a6bbd68f60a
zoujianchao <data_zjc@163.com>
2021-06-10 20:13:10 +0800

    增加分表操作

--- src/test/java/com/example/demo/ShardingDemoApplicationTests.java
+++ src/test/java/com/example/demo/ShardingDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.demo.entity.Course;
 import com.example.demo.mapper.CourseMapper;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,7 +20,7 @@ class ShardingDemoApplicationTests {
     private CourseMapper courseMapper;
 
     @Test
-    public void addCourse(){
+    public void addCourse() {
         for (int i = 0; i < 10; i++) {
             Course c = new Course();
 //            c.setCid(Long.valueOf(i));
@@ -27,6 +29,31 @@ class ShardingDemoApplicationTests {
             c.setCstatus("1");
             courseMapper.insert(c);
         }
+    }
+
+    @Test
+    public void queryCourse() {
+        List<Course> courses = courseMapper.selectList(null);
+        courses.forEach(System.out::println);
+    }
+
+    @Test
+    public void queryCourseByCid() {
+        QueryWrapper<Course> wrapper = new QueryWrapper<>();
+//        wrapper.eq("cid", 609815069894643712L);
+        wrapper.in("cid", 609815070360211456L, 609815070414737409L);
+        List<Course> courses = courseMapper.selectList(wrapper);
+        courses.forEach(System.out::println);
+
+    }
+
+    @Test
+    public void queryCourseByRang() {
+        QueryWrapper<Course> wrapper = new QueryWrapper<>();
+        wrapper.between("cid", 609815070360211456L, 609815070414737409L);
+        List<Course> courses = courseMapper.selectList(wrapper);
+        courses.forEach(System.out::println);
+
     }
 
 }
